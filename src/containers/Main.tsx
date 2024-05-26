809ad685d2ef92fd29dba06149c5236ee6da125f
Rodoro <rodoro.game@gmail.com>
2024-05-26 22:00:36 +0300

    Update bg

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <main className="bg-gray-500">
-        
+    <main>
+        <div className="fixed top-[-7rem] left-8 flex-shrink-0 w-[587px] h-[587px] opacity-[0.2] bg-[#c51fff] blur-[227px]" />
     </main>
   )
 }
